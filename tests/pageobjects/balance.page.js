0d2506a2fe55b2f9b2c021d4758d94eece76475c
Hamza Abbas <hamza.abbas.minhas@gmail.com>
2025-06-06 17:24:04 +0200

    Add Category veriffcation in expense test

--- tests/pageobjects/balance.page.js
+++ tests/pageobjects/balance.page.js
@@ -4,8 +4,16 @@ class BalancePage {
         return $('id=com.monefy.app.lite:id/textViewWholeAmount'); // Balance button locator
     }
 
+    get transactionCategoryLabel() {
+        return $('id=com.monefy.app.lite:id/textViewCategoryName'); // Transaction category label locator
+    }
+
     async balanceAmount() {
         return await this.balanceLabel.getText();
+    }
+
+    async transactionCategory() {
+        return await this.transactionCategoryLabel.getText();
     }
 }
 
--- tests/pageobjects/expense.page.js
+++ tests/pageobjects/expense.page.js
@@ -9,18 +9,31 @@ class ExpensePage {
         return $('id=com.monefy.app.lite:id/keyboard_action_button'); // Category selection button locator
     }
 
-    get noteIcon() {
-        return $('com.monefy.app.lite:id/icon'); // Note input field locator
+    get noteInput() {
+        return $('id=com.monefy.app.lite:id/noteAutocompleteTextInputLayout'); // Note input field locator
+    }
+
+    get paymentIcon() {
+        return $('id=com.monefy.app.lite:id/icon'); // Note input field locator
     }
 
     get cardInput() {
         return $('//android.widget.TextView[@resource-id="com.monefy.app.lite:id/title" and @text="Payment card"]'); // Card input field locator
     }
 
-    get dateInput() {
-        return $('id=com.monefy.app.lite:id/textViewDate'); // Date input field locator
+    get dateCalendar() {
+        return $('//android.widget.TextView[@resource-id="com.monefy.app.lite:id/textViewDate"]'); // Date input field locator
     }
-    
+
+    get editDate() {
+        return $('id=com.monefy.app.lite:id/mtrl_picker_header_toggle'); // Edit date locator
+    }
+    get dateInput() {
+        return $('//android.widget.TextView[@resource-id="com.monefy.app.lite:id/textinput_placeholder"]'); // Date input field locator
+    }
+    get dateOk() {
+        return $('//android.widget.Button[@resource-id="com.monefy.app.lite:id/confirm_button"]'); // Date ok locator
+    }
 
     get billsCategory() {
         return $('//android.widget.GridView[@resource-id="com.monefy.app.lite:id/gridViewCategories"]/android.widget.FrameLayout[1]/android.widget.LinearLayout'); // Bills category locator
@@ -35,6 +48,13 @@ class ExpensePage {
     }
 
     // Actions
+
+    async setDate(date) {
+        await this.dateCalendar.click();
+        await this.editDate.click();
+        await this.dateInput.setValue(date);
+        await this.dateOk.click();
+    }
 
 }
 
